15a59ddbeec510305d532628abce925c06ed7995
Krrish <krrishk22@iitk.ac.in>
2025-05-21 17:55:47 +0530

    added requirement file

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\khand\OneDrive\Documents\louderWorld\backend\venv
+set VIRTUAL_ENV=C:\Users\khand\OneDrive\Documents\Events\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
